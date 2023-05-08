9972d9433d868da6e7e015a4103c495c47ca8687
alinu1 <uwu1343@gmail.com>
2023-05-08 18:39:39 +0600

    image

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import './css/style.css'
 
 function App() {
   return (
-    <div className="todo">
-
+    <div className="body">
+      <div className="todo">
+      <div className="banner">
+        <h1>TODO</h1>
+      </div>
       <Header />
       <Main />
       <Footer />
-
+    </div>
     </div>
   );
 }
